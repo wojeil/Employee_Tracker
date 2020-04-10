23928427361404939a15b62ff75db144aa7adf9f
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-04-09 20:41:31 -0400

    set up my tables in db

---
+++ db/app_db.sql
@@ -0,0 +1,30 @@
+
+DROP DATABASE IF EXISTS employee_tracker_db;
+
+CREATE DATABASE employee_tracker_db;
+
+USE  employee_tracker_db;
+--Department table--
+CREATE TABLE department (
+  id INT NOT NULL AUTO_INCREMENT,
+  name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
+);
+--Role table--
+CREATE TABLE role (
+  id INT NOT NULL AUTO_INCREMENT,
+  title VARCHAR(30),
+  salary VARCHAR(30),
+  department_id INT,
+  PRIMARY KEY (id)
+);
+--Employee table--
+CREATE TABLE employee (
+  id INT NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(30),
+  last_name VARCHAR(30),
+  role_id INT,
+  manager_id INT NOT NULL
+  PRIMARY KEY (id)
+);
+
