68db6a08c7879b6e2cb8b99bdc6d6bd64169c9c7
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-04-10 22:05:46 -0400

    starting app functions and verifying connection

--- db/connection.js
+++ db/connection.js
@@ -17,7 +17,8 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId);
-  // afterConnection();
-  // afterConnections();
+  
   
 });
+
+module.exports= connection
