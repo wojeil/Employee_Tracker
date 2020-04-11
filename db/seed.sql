17d0f2feb57a11cf7bfa951ca75b3e3a1dc3c586
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-04-10 22:02:06 -0400

    Database Up and Running on MySQL

--- db/seed.sql
+++ db/seed.sql
@@ -16,8 +16,8 @@ INSERT INTO role (title, salary, department_id) values ("Sales Associate", 30000
 
 
 -- for employee table --
-INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("");
-INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Marketing");
-INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Managment");
-INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Risk Managment");
-INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Sales");
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Karam", "Helloz", 1, 1 );
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Sawyou", "Jacksas", 2, NULL);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Mounir", "Stevens", 3, 3);
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Fran", "Debois", 4, NULL );
+INSERT INTO employee (first_name, last_name, role_id, manager_id) values ("Marie", "Eid", 5, NULL);
