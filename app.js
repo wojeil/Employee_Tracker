a740f5f9ccd0a23cec34b7522d5715e9bebaeaa6
Wadih Ojeil <wadih.ojeil@gmail.com>
2020-04-10 22:15:13 -0400

    connection up and running

--- app.js
+++ app.js
@@ -2,6 +2,27 @@ var mysql = require("mysql");
 var inquirer = require('inquirer');
 
 
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root8",
+  database: "employee_tracker_db"
+});
+
+connection.connect(function(err) {
+  if (err) throw err;
+  console.log("connected as id " + connection.threadId);
+  
+  start();
+  
+});
 
 
 
